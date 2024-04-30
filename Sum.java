f05a95bb0982644ffd72311015151df452d4a6cd
phil181222 <168508479+phil181222@users.noreply.github.com>
2024-04-30 14:43:01 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum{
   public static void main(String[] args){
     int a = 5;
     int b = 10;
-    System.out.println("the sum of the two numbers is : " a + b)
+    System.out.println("the sum of the two numbers is : " a + b);
   } 
 
 }
